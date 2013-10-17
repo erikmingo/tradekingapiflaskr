b563928c585e9dbf9f32b07b4b4b43dcaeb45b32
Erik <erikmingo@gmail.com>
2013-10-17 10:47:08 -0600

    done?

--- base.py
+++ base.py
@@ -1,17 +1,14 @@
-
-#from flask import Flask, jsonify, render_template
-#import account
-from flask import Flask
+from flask import Flask, jsonify, render_template
+import account
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    #values = account.stockvalue(account.getstocksym(account.urlquery()))
-    #json = jsonify(values)
-    #json = values
-#    return render_template('home.html', json=json)
-    return "lol"
+    values = account.stockvalue(account.getstocksym(account.urlquery()))
+    json = jsonify(values)
+    json = values
+    return render_template('home.html', json=json)
 
 
 #@app.route("/stocks/")
